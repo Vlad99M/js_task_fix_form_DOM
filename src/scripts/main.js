d0ac875c97127d136b4c877308dcd1ac1b3db20e
w11ue <https://github.com/Vlad99M>
2026-07-03 23:43:10 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,17 @@
 'use strict';
 
 // write code here
-const form = document.querySelector('form');
-const inputs = form.querySelectorAll('input');
+const inputs = document.querySelectorAll('input');
 
 inputs.forEach((input) => {
   const label = document.createElement('label');
 
   label.className = 'field-label';
-  label.setAttribute('for', input.id);
+  label.htmlFor = input.id;
 
-  const fieldName = input.name;
-  let displayName = fieldName.replace(/([A-Z])/g, ' $1').trim();
-
-  displayName = displayName.charAt(0).toUpperCase() + displayName.slice(1);
+  const displayName = input.name
+    .replace(/([A-Z])/g, ' $1')
+    .replace(/^./, (char) => char.toUpperCase());
 
   label.textContent = displayName;
   input.placeholder = displayName;
